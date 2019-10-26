953a2d158fcedfbce541d35d8e976b9abbbff189
infiltration-x <52758100+infiltration-x@users.noreply.github.com>
2019-10-26 13:01:22 +0530

    Add files via upload

---
+++ css/s_main.js
@@ -0,0 +1,13 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "265px";
+
+  document.getElementById("main").style.opacity = "0.5";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+
+  document.getElementById("main").style.opacity = "1";
+}
